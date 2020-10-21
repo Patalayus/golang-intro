3c0fb45d3fd84223649c8d98981fb75d186a8990
Patalayus <lukecharles222@gmail.com>
2020-10-21 19:26:13 +0100

    moving onto range

--- src/github.com/patalayus/08_maps/main.go
+++ src/github.com/patalayus/08_maps/main.go
@@ -5,11 +5,16 @@ import "fmt"
 
 func main() {
 	//Define map
-	emails := make(map[string]string)
+	//emails := make(map[string]string)
 
 	//Assign key values
-	emails["Bob"] = "bob@gmail.com"
-	emails["Travis"] = "travis@gmail.com"
+	//emails["Bob"] = "bob@gmail.com"
+	//emails["Travis"] = "travis@gmail.com"
+	//emails["Mike"] = "mike@gmail.com"
+
+	//declare map and add key values
+	emails := map[string]string{"Bob": "bob@gmail.com", "travis": "travis@gmail.com"}
+
 	emails["Mike"] = "mike@gmail.com"
 
 	fmt.Println(emails)
