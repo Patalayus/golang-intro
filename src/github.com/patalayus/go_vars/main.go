72e943297b472d57b3d09507dc25aee111899434
Patalayus <lukecharles222@gmail.com>
2020-10-21 17:48:57 +0100

    go progress more

--- src/github.com/patalayus/go_vars/main.go
+++ src/github.com/patalayus/go_vars/main.go
@@ -3,11 +3,26 @@ package main
 
 import "fmt" //formatter module
 
+
+
 func main() {
 	//Using var
 	var name string = "Alex"
-	var age = 37
+	//datatype declaration done below
+	var age int32 = 37
+	//as this is var, we can redefine, unlike const
+	var isCool = true
 
-	fmt.Println(name, age)
-	fmt.Printf("")
+	//shorthand definition
+	name2 := "Lewis"
+	//shorthand for float64
+	size := 1.3
+
+	isCool = false
+
+	//declaring multiple variables
+	name, email := "Alex", "alex@gmail.com"
+
+	fmt.Println(name, age, isCool, name2, email, size)
+	fmt.Printf("%T\n",email)
 }
